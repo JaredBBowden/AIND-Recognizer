f6770cf88ddaf9f78760cd5faf93f5414ddaacb5
Jared <jaredbbowden@gmail.com>
2017-07-21 12:20:21 -0500

    Submission version

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -7,6 +7,7 @@ from hmmlearn.hmm import GaussianHMM
 from sklearn.model_selection import KFold
 from asl_utils import combine_sequences
 
+import pdb
 
 class ModelSelector(object):
     '''
@@ -76,8 +77,42 @@ class SelectorBIC(ModelSelector):
         """
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        # Variables to hold update scores
+        best_bic = math.inf
+        best_model = GaussianHMM()
+
+        # Iterate across a range of model states
+        for num_hidden_states in range(self.min_n_components, self.max_n_components + 1):
+
+            try:
+                # Fit a model based on state
+                model = GaussianHMM(n_components = num_hidden_states, n_iter = 100)
+                model.fit(self.X, self.lengths)
+
+                # Values needed to for BIC
+                # From the slides: http://www2.imm.dtu.dk/courses/02433/doc/ch6_slides.pdf
+                # BIC = −2 log L + p log N,
+
+                # Get WITHIN sample logL
+                logL = model.score(self.X, self.lengths)
+
+                # Compute the number of parameters
+                num_parameters = num_hidden_states * num_hidden_states + 2 * num_hidden_states * len(self.X[0]) - 1
+
+                # Compute overall BIC formula
+                current_bic = (-2) * logL + num_parameters * math.log(len(self.X))
+
+                # Control flow to update BIC score
+                if current_bic <= best_bic:
+                    best_model, best_bic = model, current_bic
+
+                else:
+                    continue
+
+            except:
+                continue
+
+        return best_model
 
 
 class SelectorDIC(ModelSelector):
@@ -92,8 +127,62 @@ class SelectorDIC(ModelSelector):
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        # I'm going to use the same general skeleton I had been using for
+        # the previous methods
+        # Variables to hold update scores
+        best_dic = -math.inf
+        best_model = GaussianHMM()
+
+        # Iterate across a range of model states
+        for num_hidden_states in range(self.min_n_components, self.max_n_components + 1):
+
+            try:
+
+                # Fit a model based on state
+                model = GaussianHMM(n_components = num_hidden_states, n_iter = 100)
+                model.fit(self.X, self.lengths) # What are we fitting on?
+                #model = self.base_model(num_hidden_states)
+
+                # Compute elements we need. Full equation:
+                # DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
+                sigma_scores = 0
+                M = 0
+
+                #pdb.set_trace()
+
+                # FIXME
+                # Get WITHIN sample logL
+                logL = model.score(self.X, self.lengths)
+
+                # Compute sum(logL) for all words that are not the X[i]
+                for word in self.hwords:
+
+                    if word != self.this_word:
+
+                        # Pull values to score this word
+                        temp_X, temp_length = self.hwords[word]
+
+                        # Score
+                        sigma_scores += model.score(temp_X, temp_length)
+                        M += 1
+
+                # Now we compute DIC
+                current_dic = logL - (1.0 / (float(M)) * sigma_scores)
+
+                # Control flow to update DIC score
+                if current_dic >= best_dic:
+                    #print("We're updating scores!")
+                    best_model, best_dic = model, current_dic
+
+                else:
+                    #print("We're NOT updating scores...")
+                    continue
+
+            except:
+                #print("We missed the try block")
+                continue
+
+        return best_model
 
 
 class SelectorCV(ModelSelector):
@@ -104,5 +193,65 @@ class SelectorCV(ModelSelector):
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection using CV
-        raise NotImplementedError
+        #pdb.set_trace()
+
+        # Variable to hold the best scores-and-model across CV iterations
+        best_score = -math.inf
+
+        # Initialize scikit hmm object with default parameters
+        best_model = GaussianHMM()
+
+        # Initialize values for CV split object.
+        # FIXME This could be achieved more eloquently: rough adjustments to
+        # allow code to account for scenarios encountered within Recognizer data.
+        if len(self.sequences) <= 2:
+            splits = 2
+        else:
+            splits = 3
+
+        split_method = KFold(n_splits = splits)
+
+        # Iterate through states: for each state cross-validate n times.
+        # Language and structure based on the CV snippet from the notebook
+        # The execution snippet provides man and min components.
+        for num_hidden_states in range(self.min_n_components, self.max_n_components + 1):
+            try:
+
+                # Return index values for fold splits
+                # FIXME should there be a random parameter here?
+                for cv_train_idx, cv_test_idx in split_method.split(self.sequences):
+
+                    try:
+                        #pdf.set_trace()
+
+                        # To USE the index that we get from our folds we need to use the
+                        # provided function.
+
+                        # For training data
+                        X_train, X_train_lengths = combine_sequences(cv_train_idx, self.sequences)
+
+                        # For test data
+                        X_test, X_test_lengths = combine_sequences(cv_test_idx, self.sequences)
+
+                        # Fit the model on the fold data (training) and current number
+                        # of states.
+                        # Note that the number of iterations use here is a carry over
+                        # from the notebook.
+                        model = GaussianHMM(n_components = num_hidden_states, n_iter = 1000)
+                        model.fit(X_train, X_train_lengths)
+
+                        # Return score on the test data
+                        logL = model.score(X_test, X_test_lengths)
+
+                        # Control flow to test for high-scores
+                        if logL >= best_score:
+
+                            best_model, best_score = model, logL
+
+                    except:
+                        continue
+
+            except:
+                continue
+
+        return best_model
--- my_recognizer.py
+++ my_recognizer.py
@@ -1,6 +1,8 @@
 import warnings
 from asl_data import SinglesData
+import math
 
+import pdb
 
 def recognize(models: dict, test_set: SinglesData):
     """ Recognize test word sequences from word models set
@@ -18,8 +20,38 @@ def recognize(models: dict, test_set: SinglesData):
            ['WORDGUESS0', 'WORDGUESS1', 'WORDGUESS2',...]
    """
     warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+    # Initialize variables to hold values that will be returned
     probabilities = []
-    guesses = []
-    # TODO implement the recognizer
-    # return probabilities, guesses
-    raise NotImplementedError
+    guess = []
+
+    # Iterate over the test set
+    for X_test, X_length in list(test_set.get_all_Xlengths().values()):
+
+        # Hold values through iterations
+        word_score = -math.inf
+        word_guess = None
+        probability_dictionary = {}
+
+        # Iterate over elements of trained models
+        for word, model in models.items():
+
+            # The word associated with the highest score is the best guess
+            try:
+                temp_score = model.score(X_test, X_length)
+
+                # Save all score results (by word) to a dictionary
+                probability_dictionary[word] = temp_score
+
+                if temp_score >= word_score:
+                    word_score, word_guess = temp_score, word
+
+            except:
+                # Account for the condition where we are unable to generate
+                # a score
+                probability_dictionary[word] = -math.inf
+
+        probabilities.append(probability_dictionary)
+        guess.append(word_guess)
+
+    return probabilities, guess
